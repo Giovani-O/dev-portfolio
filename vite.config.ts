1e991a6eb69b9007011bd510b3510f3eb3569b10
Giovani <giovani98@proton.me>
2026-04-26 00:12:42 -0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/giovani-o/',
+  base: '/dev-portfolio/',
   plugins: [react()],
   test: {
     globals: true,
